59164a37cba2f3539c66b73bb79004c2ae7a6034
pjueon <bluegbgb@gmail.com>
2019-10-03 23:32:56 +0900

    주석 수정

--- source codes/Logger.h
+++ source codes/Logger.h
@@ -13,7 +13,7 @@ public:
 	Logger(MainMachine* const mainMachine, const string moduleName);
 	~Logger();
 
-	// DB에 기록하기(DBComunication을 사용)
+	// DB에 기록하기(DBTool의 기능을 사용)
 	// 파일에 기록
 	// UI에 띄울 메시지 작성 
 	// etc
