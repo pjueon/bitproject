a4708227a6a7cdce964b40cb4cf544db701ea537
pjueon <bluegbgb@gmail.com>
2019-12-19 22:19:41 -0800

    add string_test

--- string_test/src/test.cpp
+++ string_test/src/test.cpp
@@ -14,7 +14,7 @@ using namespace std;
 int main(int argc, char **argv){
 	ros::init(argc, argv, "test_node");
 	ros::NodeHandle n;
-	auto testpub = n.advertise<std_msgs::String>("/test_topic", 1);
+	auto testpub = n.advertise<std_msgs::String>("/test_topic", 1000);
 
 
 	constexpr auto filename = "/home/jetbot/catkin_ws/src/bitproject/ImageDB/booknames.txt";
@@ -27,21 +27,20 @@ int main(int argc, char **argv){
 
 	vector<string> lines;
 	string line;
+	std_msgs::String msg;
 
 	while (getline(fin, line)) {
 		lines.push_back(line);
 	}
 
 	cerr << "wait" << endl;
-	ros::Duration(1.0).sleep();
 
 	for(const auto& bookname : lines){
-		std_msgs::String msg;
 		msg.data = bookname;
 		testpub.publish(msg);
 		cout << "published!";
 		cout << msg.data << endl;
+		ros::Duration(1.0).sleep();
 	}
-
 	return 0;
 }
