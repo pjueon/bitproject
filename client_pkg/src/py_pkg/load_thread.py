ee07718c52d4c23e28d2b26f97bf6c4e89454278
kim <kjk7455@gmail.com>
2019-12-23 17:14:51 +0900

    change

--- client_pkg/src/py_pkg/load_thread.py
+++ client_pkg/src/py_pkg/load_thread.py
@@ -3,15 +3,19 @@ import rospy
 import cv2
 import numpy as np
 import time
+import os
 
 from PySide2.QtCore import QThread, Signal, Slot
 from PySide2.QtGui import QPixmap, QImage
+
+def fixpath(path):
+    return os.path.abspath(os.path.expanduser(path))
 
 class Loader(QThread):
     loading = Signal(object)
     def __init__(self, parent = None):
         super(Loader, self).__init__()
-        self.num = None
+        self.num = 360
         self.text = "Loading..."
         self.loading_flag = True
 
@@ -22,8 +26,15 @@ class Loader(QThread):
         print("============================= End loading Thread ===============================\n\n")
 
     def view_load(self):
-        self.num = 1
+        newImage = cv2.imread(fixpath("~/catkin_ws/src/bitproject/client_pkg/src/py_pkg/image/loading.png"), cv2.IMREAD_COLOR)
+
+        newImage = cv2.cvtColor(newImage, cv2.COLOR_BGR2RGB)
+        ros_Height, ros_Width, channel = newImage.shape
+        bytesPerLine = 3 * ros_Width
+        Center = (int(ros_Width/2), int(ros_Height/2))
+        newImage = newImage.astype('uint8')
         while self.loading_flag:
+            '''
             if self.num > 10:
                 self.num = 1
             Loading = np.ones((70, self.num * 38, 3), dtype = np.float32) * 255
@@ -44,3 +55,32 @@ class Loader(QThread):
 
             time.sleep(0.5)
             self.num += 1
+            '''
+            if self.num < 0:
+                self.num = 360
+            #newImage = cv2.cvtColor(newImage, cv2.COLOR_BGR2RGB)
+            angle = self.num
+
+            Rotated = cv2.getRotationMatrix2D(Center, angle, .3)
+
+            #abs_cos = abs(Rotated[0,0])
+            #abs_sin = abs(Rotated[0,1])
+
+            #bound_w = int(ros_Height * abs_sin + ros_Width * abs_cos)
+            #bound_h = int(ros_Height * abs_cos + ros_Width * abs_sin)
+
+            # subtract old image center (bringing image back to origo) and adding the new image center coordinates
+            #Rotated[0, 2] += bound_w/2 - Center[0]
+            #Rotated[1, 2] += bound_h/2 - Center[1]
+
+            # rotate image with the new bounds and translated rotation matrix
+            RotatedMaker = cv2.warpAffine(newImage,
+                                           Rotated,
+                                           (ros_Width, ros_Height),
+                                           borderValue = (255, 255, 255)
+                                           )
+            qt_image = QImage(RotatedMaker, ros_Width, ros_Height, bytesPerLine, QImage.Format_RGB888)
+            #print(angle)
+            self.loading.emit(qt_image)
+            time.sleep(0.05)
+            self.num -= 30
